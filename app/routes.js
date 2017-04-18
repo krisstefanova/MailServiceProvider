51bbed203a33977314856089a34e76a233801ce6
Kristina Stefanova <kris.stefanova@gmail.com>
2017-04-18 10:30:51 +1000

    Trying to fix some strange formatting mismatch that is visible in GIT only and otherwise looking good in my text editor

--- app/routes.js
+++ app/routes.js
@@ -2,23 +2,22 @@
 let emailProvider = require('./sendEmailController');
 
 module.exports = function(app) {
-
 	app.get('/', function(req, res) {
 		res.render('index.ejs'); // load the index.ejs file
 	});
 
 	app.post('/', function(req, res) {
-    if (blankRequiredField(req)) {
+		if (blankRequiredField(req)) {
       return res.render('index', {error: "Please, input all required (*) fields"});
     }
-    emailProvider.sendDataToEmailProvider(req.body, function(error, data) {
-      if (error) {
+		emailProvider.sendDataToEmailProvider(req.body, function(error, data) {
+			if (error) {
         res.render('index.ejs', {error: "Excuse us, our email provider is unavailable at the moment"});
-      } else {
+			} else {
         res.render('index.ejs', {success: "You successfully sent an email"});
-      }
-    })
-  })
+			}
+		})
+	})
 }
 
 /*
